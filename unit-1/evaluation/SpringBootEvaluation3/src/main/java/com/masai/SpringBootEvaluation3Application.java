d20cd09e477478cd639862f99f601fce1dfd4f4d
WASHIM RAZA <96364192+washimraza1234@users.noreply.github.com>
2022-08-18 12:21:17 +0530

    Add files via upload

---
+++ unit-1/evaluation/SpringBootEvaluation3/src/main/java/com/masai/SpringBootEvaluation3Application.java
@@ -0,0 +1,13 @@
+package com.masai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEvaluation3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEvaluation3Application.class, args);
+	}
+
+}
