f3a82618fbf8f90629bf33589a2b7cbe31312801
WASHIM RAZA <96364192+washimraza1234@users.noreply.github.com>
2022-11-19 19:07:11 +0530

    Add files via upload

---
+++ Revision/assignment2/Main.java
@@ -0,0 +1,16 @@
+package com.masai.assignment2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		//Animal animal= new Cat();
+		Animal animal= new Dog();
+		animal.eat();
+		animal.makNoise();
+		animal.sleep();
+		
+		
+		
+		
+	}
+}
